67454b7c5bf11e57884e673e51723a3a31053ac4
Blessing Kodze <80010688+Chinxeleer@users.noreply.github.com>
2024-04-13 12:53:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = "World From Thapelo's Computer here";
+  const value = "World From Thapelo's Computer here and from Blessing's computer here";
   return <div>Hello {value}</div>;
 }
 
