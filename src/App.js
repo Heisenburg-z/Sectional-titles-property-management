ca1a56a7993938305b41ebfc4fa1de049a258f27
Heisenburg-z <2603646@students.wits.ac.za>
2024-04-15 13:53:23 +0200

    login and signup functionality done ✅

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 import React from "react";
 import LandingPage from "./Pages/Home/LandingPage/LandingPage";
 import {
-  createBrowserRouter,
-  Route,
-  createRoutesFromElements,
-  RouterProvider,
+	createBrowserRouter,
+	Route,
+	createRoutesFromElements,
+	RouterProvider,
 } from "react-router-dom";
 import LoginForm from "./Pages/Login/LoginForm";
 import About from "./Pages/About/About";
@@ -15,26 +15,27 @@ import StaffPage from "./Pages/Staff/StuffPage";
 import Hero from "./Pages/Home/Hero/Hero";
 import Error404 from "./Pages/Errors/Error404";
 import SignUpForm from "./Pages/SignUP/SignUpForm";
+import ResidentsPage from "./Pages/Residents/ResidentsPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<LandingPage />}>
-        <Route path="" element={<Hero />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="*" element={<Error404 />} />
-      </Route>
-      <Route path="/admin" element={<AdminPage />}>
-        <Route path="signup" element={<SignUpForm />} />
-      </Route>
-      <Route path="/resident" element={<LoginForm />} />
-      <Route path="/staff" element={<StaffPage />} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route>
+			<Route path="/" element={<LandingPage />}>
+				<Route path="" element={<Hero />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/login" element={<LoginForm />} />
+				<Route path="*" element={<Error404 />} />
+			</Route>
+			<Route path="/admin" element={<AdminPage />}>
+				<Route path="signup" element={<SignUpForm />} />
+			</Route>
+			<Route path="/resident" element={<ResidentsPage />} />
+			<Route path="/staff" element={<StaffPage />} />
+		</Route>
+	)
 );
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
