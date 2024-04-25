972b3c56beeeee17e3df63aa86353735bca161f6
Bokang <bokangmolepo03@gmail.com>
2024-04-25 17:54:06 +0200

    staff page content added

--- src/Pages/Staff/Components/SideBar/StaffSideBar.js
+++ src/Pages/Staff/Components/SideBar/StaffSideBar.js
@@ -63,7 +63,7 @@ function StaffSideBar({ className }) {
                   src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="profile"
                 />
-                <p className="item-padding">Bokang Molepo</p>
+                <p className="item-padding">Jim Kalake</p>
               </span>
             </NavLink>
           </li>
