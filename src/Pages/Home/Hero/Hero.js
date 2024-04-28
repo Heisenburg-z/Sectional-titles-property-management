16a7361594710c85a7b9df27dc6357223dffdc23
souoGael <160495329+souoGael@users.noreply.github.com>
2024-04-28 14:37:10 +0200

    Update Hero.js

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -4,7 +4,7 @@ import './Hero.css'
 function Hero() {
   return <>
     <h1 className="welcometxt"> Welcome </h1>
-    <h3 className="welcomeMsg">Sectional Propeties Managements</h3>
+    <h3 className="welcomeMsg">Sectional Properties Management</h3>
   </>  
 }
 
