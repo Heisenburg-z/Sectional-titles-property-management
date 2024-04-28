ccb30b88cd94e787852f709a9e92b22332362c7c
Thapelo Ndlovu <133436982+Heisenburg-z@users.noreply.github.com>
2024-04-29 00:11:47 +0200

    Update Hero.test.js

--- src/Pages/Home/__tests__/Hero.test.js
+++ src/Pages/Home/__tests__/Hero.test.js
@@ -1,4 +1,28 @@
-test("example test", () => {
-  // Replace this with your actual test logic
-  expect(true).toBe(true);
+/ Hero.test.js
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Hero from "../Hero/Hero.js";
+
+describe('Hero Component', () => {
+  test('renders welcome text', () => {
+    render(<Hero />);
+    const welcomeText = screen.getByText('Welcome');
+    expect(welcomeText).toBeInTheDocument();
+  });
+
+  test('renders welcome message', () => {
+    render(<Hero />);
+    const welcomeMessage = screen.getByText('Sectional Properties Management');
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+
+  test('applies correct CSS classes to elements', () => {
+    render(<Hero />);
+    const welcomeText = screen.getByText('Welcome');
+    const welcomeMessage = screen.getByText('Sectional Properties Management');
+
+    expect(welcomeText).toHaveClass('welcometxt');
+    expect(welcomeMessage).toHaveClass('welcomeMsg');
+  });
 });
