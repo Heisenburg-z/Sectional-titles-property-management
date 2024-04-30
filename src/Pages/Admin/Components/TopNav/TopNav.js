5fa306d6718d4dabfa6478aec55505d29884ff1c
Blessing Kodze <blessingeleer.programming@gmail.com>
2024-04-30 22:22:01 +0200

    changed admin styles

--- src/Pages/Admin/Components/TopNav/TopNav.js
+++ src/Pages/Admin/Components/TopNav/TopNav.js
@@ -2,19 +2,19 @@ import { useLocation } from "react-router-dom";
 import "./TopNav.css";
 import { IoNotifications } from "react-icons/io5";
 
-function TopNav({ className }) {
-	const location = useLocation();
-	const displayName = location.pathname.split("/");
-	console.log(displayName);
+function TopNav() {
+  const location = useLocation();
+  const displayName = location.pathname.split("/");
+  console.log(displayName);
 
-	return (
-		<header className={className}>
-			<h2 className="header-title">
-				{displayName[1].toUpperCase()}/{displayName[2].toUpperCase()}
-			</h2>
-			<IoNotifications size={30} className="header-icon" />
-		</header>
-	);
+  return (
+    <header className="header">
+      <h2 className="header-title">
+        {displayName[1].toUpperCase()}/{displayName[2].toUpperCase()}
+      </h2>
+      <IoNotifications size={30} className="header-icon" />
+    </header>
+  );
 }
 
 export default TopNav;
