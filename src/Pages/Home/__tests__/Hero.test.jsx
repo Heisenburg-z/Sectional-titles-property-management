ce66e168c3c6d9b7525e338f7c5ae948fb5e11af
Blessing Kodze <blessingeleer.programming@gmail.com>
2024-05-03 16:44:13 +0200

    removed some dependencies and finished the navbar test

--- src/Pages/Home/__tests__/Hero.test.jsx
+++ src/Pages/Home/__tests__/Hero.test.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import "@testing-library/jest-dom/extend-expect"
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Hero from "../Hero/Hero.js";
 
-describe('Hero Component', () => {
-  test('renders welcome text', () => {
+describe("Hero Component", () => {
+  test("renders welcome text", () => {
     render(<Hero />);
-    const welcomeText = screen.getByText('Welcome');
+    const welcomeText = screen.getByText("Welcome");
     expect(welcomeText).toBeInTheDocument();
   });
 
-  test('renders welcome message', () => {
+  test("renders welcome message", () => {
     render(<Hero />);
-    const welcomeMessage = screen.getByText('Sectional Properties Management');
+    const welcomeMessage = screen.getByText("Sectional Properties Management");
     expect(welcomeMessage).toBeInTheDocument();
   });
 });
---
+++ src/Pages/Home/__tests__/Nav.test.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "../Navbar/Nav";
+import { AuthProvider } from "../../../utils/auth.js";
+import { BrowserRouter } from "react-router-dom";
+jest.mock("../Navbar/Nav.css", () => ({}));
+
+describe("Renders the Nav", () => {
+  test("renders the logo", () => {
+    render(
+      <BrowserRouter>
+        <AuthProvider>
+          <Nav />
+        </AuthProvider>
+      </BrowserRouter>,
+    );
+    const welcomeText = screen.getByText("Logo");
+    expect(welcomeText).toBeInTheDocument();
+  });
+
+  test("renders the about us link", () => {
+    render(
+      <BrowserRouter>
+        <AuthProvider>
+          <Nav />
+        </AuthProvider>
+      </BrowserRouter>,
+    );
+    const welcomeMessage = screen.getByText(/About Us/i);
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+});
