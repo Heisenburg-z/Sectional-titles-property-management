f289b4837e81a7efb188d421cc7715025710714f
Jim Kalake <jimkalake@gmail.com>
2024-05-07 12:57:11 +0200

    api that logs in maintenance issue complete

---
+++ api/src/functions/logMaintenance.js
@@ -0,0 +1,18 @@
+const { collection, addDoc } = require('firebase/firestore');
+const db = require('./firebaseDB');
+const { app } = require('@azure/functions');
+
+app.http('logMaintenance', {
+    methods: ['POST'],
+    route: "property/resident/maintenance",
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+    const data = await request.json(); // Assuming you send the updated data in the request body
+
+    try {
+        db.collection("maintenance").add(data);
+    } catch (error) {
+      return { message: "Error adding maintenance issue: " + error.message };
+    }
+  },
+});
--- src/Pages/Residents/ResidentTabs/Reports/ResidentsMaintenance.js
+++ src/Pages/Residents/ResidentTabs/Reports/ResidentsMaintenance.js
@@ -1,20 +1,65 @@
-import React from 'react'
+import React, {  useState } from 'react'
 import './ResidentsMaintenance.css';
 function ResidentsMaintenance() {
+  const [dropdownValue, setDropdownValue] = useState("");
+
+  const handleDropdownChange = (e) => {
+		setDropdownValue(e.target.value);
+	};
+  const [roomNo, setRoomNo] = useState("");
+  const [date, setdate] = useState("");
+  const [description, setDescription] = useState("");
+
+  
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`/api/property/resident/maintenance`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          maintenanceType: dropdownValue,
+          roomNumber: roomNo,
+          date: date,
+          Description: description,
+          Status: "Open",
+        }),
+      });
+      if(response.ok){
+        console.log("Request sent" + response);
+        setDropdownValue("");
+        setRoomNo("");
+        setdate("");
+        setDescription("")
+      }
+      
+    } catch (error) {
+      console.error("Error adding maintenance problem:", error);
+    }
+
+    
+  };
+
+
+
+
   return (
     <section>
-        <form action="" className='form'>
-            <select name="" id="" className='input-field'>
+        <form action="" className='form' onSubmit={handleOnSubmit}>
+            <select name="" id="" className='input-field' onChange={handleDropdownChange} value={dropdownValue}>
                 <option value="">Type of Maintenance</option>
                 <option value="Water">Water</option>
                 <option value="Electricity">Electricity</option>
                 <option value="Furniture">Furniture</option>
             </select>
             <label htmlFor="">Room Number:</label>
-            <input type="text" className='input-field' id="" />
-            <input type="date" className='input-field'/>
+            <input type="text" className='input-field' id="" value={roomNo} onChange={(e)=> setRoomNo(e.target.value)}/>
+            <input type="date" className='input-field' value={date} onChange={(e)=> setdate(e.target.value)}/>
             <label htmlFor="">Description</label>
-            <textarea className='input-field' id="" rows="5" col="40"></textarea>
+            <textarea className='input-field' id="" rows="5" col="40" value={description} 
+            onChange={(e)=>setDescription(e.target.value)}></textarea>
             <button type="submit">Submit</button>
 
         </form>
