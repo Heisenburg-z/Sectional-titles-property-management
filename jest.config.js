38a8e0609e6a43261b24bbeb654eb66224cb571d
Bokang <bokangmolepo03@gmail.com>
2024-05-19 13:24:16 +0200

    edited jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["src/Pages/**/*.{js,jsx}"],
-  coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy'
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+  setupFilesAfterEnv: []
 };
