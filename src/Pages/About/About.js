cc485bab4a30edca9d7651b4f75bc016a3541346
Blessing Kodze <blessingeleer.programming@gmail.com>
2024-05-20 12:36:32 +0200

    styling about us...

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,14 +2,29 @@ import React from "react";
 
 function About() {
   return (
-    <section className="container flex flex-col justify-center">
-      <h2 className="text-3xl font-bold mb-20 text-center">About Us</h2>
-      <p className="text-slate-800 font-mono text-center font-medium">
-        Non in in sint fugiat reprehenderit exercitation proident eu ullamco
-        minim sunt sint. Cupidatat tempor consectetur dolore nulla duis esse
-        nulla Lorem elit nisi dolor ullamco quis.
-      </p>
-    </section>
+    <>
+      <section className="container flex flex-col justify-center">
+        <h2 className="text-5xl font-bold mb-20 text-center">
+          Sectional Titles & Property Management
+        </h2>
+        <p className="text-slate-800 font-mono text-center font-medium w-3/5 mx-auto">
+          <span className="text-3xl font-bold text-sky-400">
+            Sectional Title{" "}
+          </span>
+          describes the separate ownership of a unit within a group-owned
+          complex or development. Simply put, the term refers to a complex with
+          flats, townhouses or apartments that has{" "}
+          <span className="text-sky-400 underline">multiple owners</span>; each
+          owning a section of the overall property. The collective of owners
+          typically elect a body corporate made up of some of the owners to take
+          responsibility for some of the tasks required to maintain the
+          property. This includes overseeing the enforcement of rules and
+          regulations, the security, upkeep and maintenance of the property, and
+          managing communication with the owners, renters and related parties
+          among other things.
+        </p>
+      </section>
+    </>
   );
 }
 
