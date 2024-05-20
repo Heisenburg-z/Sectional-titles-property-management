814b2890e07aca7371efa08aba305a976796b80e
Heisenburg <2603646@students.wits.ac.za>
2024-05-20 11:21:04 +0200

    Weather API added

--- api/src/functions/getNotifications.js
+++ api/src/functions/getNotifications.js
@@ -1,21 +1,20 @@
 const db = require('./firebaseDB');
 const { app } = require("@azure/functions");
 
-
 app.http("getNotifications", {
-	route: "property/resident/dashboard/{id}",
-	methods: ["GET"],
-	authLevel: "anonymous",
-	handler: async (request, context) => {
-		const uid = request.params.id;
+  route: "property/resident/dashboard/{id}",
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    const uid = request.params.id;
 
-		console.log(uid);
-		const cityRef = db.collection("notifications").doc(uid);
-		const doc = await cityRef.get();
-		if (doc.exists) {
-			return { body: JSON.stringify(doc.data()) };
-		} else {
-			return { message: "Does not exist" };
-		}
-	}
+    console.log(uid);
+    const cityRef = db.collection("notifications").doc(uid);
+    const doc = await cityRef.get();
+    if (doc.exists) {
+      return { body: JSON.stringify([doc.data()]) }; // Returning an array to maintain consistency
+    } else {
+      return { body: JSON.stringify([]) }; // Returning an empty array if no document is found
+    }
+  }
 });
