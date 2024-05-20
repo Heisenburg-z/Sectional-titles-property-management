e87fb0830741b09bf64fa02683edf9b27a252f06
Blessing Kodze <blessingeleer.programming@gmail.com>
2024-05-20 14:26:56 +0200

    corrected the getReports api

--- api/src/functions/getReports.js
+++ api/src/functions/getReports.js
@@ -1,14 +1,14 @@
-const db = require('./firebaseDB');
+const db = require("./firebaseDB");
 
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
 
-app.http("getFines", {
+app.http("getReports", {
 	route: "property/resident/reports/{id}",
 	methods: ["GET"],
 	authLevel: "anonymous",
 	handler: async (request, context) => {
 		try {
-            const uid = request.params.id;
+			const uid = request.params.id;
 
 			const AdminRef = db.collection("maintenance").where("userId", "==", uid);
 			const snapshot = await AdminRef.get();
@@ -26,5 +26,5 @@ app.http("getFines", {
 		} catch (error) {
 			return { status: 500, body: error.message };
 		}
-	}
+	},
 });
