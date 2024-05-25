a83a49096e0dab9349dc561165a7296daa94a2d8
Bokang <bokangmolepo03@gmail.com>
2024-05-26 01:36:10 +0200

    Fix import error

--- src/Pages/Residents/Components/SideBar/ResidentsSideBars.js
+++ src/Pages/Residents/Components/SideBar/ResidentsSideBars.js
@@ -1,7 +1,7 @@
 import "../SideBar/ResidentsSideBar.css";
 import { NavLink } from "react-router-dom";
 import { FaBars, FaToolbox } from "react-icons/fa6";
-import { GiTakeMyMoney } from "react-icons/gi";
+//import { GiTakeMyMoney } from "react-icons/gi";
 import { TbReportSearch } from "react-icons/tb";
 import { GiExitDoor } from "react-icons/gi";
 import { useAuth } from "../../../../utils/auth.js";
