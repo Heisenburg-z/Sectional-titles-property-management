b29dfa65618857921a078a9e50157adef1a9c1d1
Blessing Kodze <80010688+Chinxeleer@users.noreply.github.com>
2024-05-26 00:30:19 +0200

    Update FinesDashboard.js

--- src/Pages/Admin/AdminTabs/Fines/FinesDashboard.js
+++ src/Pages/Admin/AdminTabs/Fines/FinesDashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Oval } from "react-loader-spinner";
 
