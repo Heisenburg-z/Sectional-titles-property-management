4ef312dd4d3f425d6a4604f0ed87e50c45fce7f1
souoGael <2494554@students.wits.ac.za>
2024-05-26 13:41:17 +0200

    GT

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,17 +4,16 @@ function About() {
   return (
     <>
       <section className="container flex flex-col justify-center">
-        <h2 className="text-5xl font-bold mb-20 text-center">
+        <h1 className="text-sky-400 mb-20 text-center">
           Sectional Titles & Property Management
-        </h2>
-        <p className="text-slate-800 font-mono text-center font-medium w-3/5 mx-auto">
-          <span className="text-3xl font-bold text-sky-400">
+        </h1>
+        <h2 className="text-3xl text-slate-400 text-center text-justify">
+          <span className="font-bold text-sky-400">
             Sectional Title{" "}
           </span>
           describes the separate ownership of a unit within a group-owned
           complex or development. Simply put, the term refers to a complex with
-          flats, townhouses or apartments that has{" "}
-          <span className="text-sky-400 underline">multiple owners</span>; each
+          flats, townhouses or apartments that has multiple owners; each
           owning a section of the overall property. The collective of owners
           typically elect a body corporate made up of some of the owners to take
           responsibility for some of the tasks required to maintain the
@@ -22,7 +21,7 @@ function About() {
           regulations, the security, upkeep and maintenance of the property, and
           managing communication with the owners, renters and related parties
           among other things.
-        </p>
+        </h2>
       </section>
     </>
   );
